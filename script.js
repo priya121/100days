946216f368a1603c4894f1fcd7c952312aa0a735
priya121 <priya121@github.com>
2016-01-29 17:41:24 +0000

    First 5 posts.

---
+++ script.js
@@ -0,0 +1,22 @@
+var feed = new Instafeed({
+    get: 'user',
+    userId: '26981234',
+    accessToken: '26981234.1677ed0.f2593aabfac04e08940ab8f636676206',
+    resolution: 'standard_resolution',
+    template: '<li> <h2 class="day-number"> </h2> <a href="{{link}}"><img src="{{image}}" /></a></li>',
+    filter: function(image) {
+        return image.tags.indexOf('100days100photos') >=0;
+    },
+    after: function() {
+        addDayNumber();
+    }
+});
+
+function addDayNumber() {
+    var allPhotos = document.getElementsByClassName("day-number");
+    for (var i = 0; i < allPhotos.length ; i++) {
+        allPhotos[i].innerHTML = "Day " + (allPhotos.length - i);
+    }
+}
+
+feed.run();
